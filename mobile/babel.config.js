f15095311ebd88f47357aee35dd70478409862d4
DreamDeshi <desihgan@gmail.com>
2026-06-24 01:48:26 +0800

    Phase 3 complete - mobile app working on Expo Go SDK 54

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,7 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    // react-native-reanimated/plugin MUST be the last plugin in the list.
-    plugins: ["react-native-reanimated/plugin"],
+    // Reanimated 4 moved its Babel plugin into react-native-worklets.
+    // It MUST be the last plugin in the list.
+    plugins: ["react-native-worklets/plugin"],
   };
 };
